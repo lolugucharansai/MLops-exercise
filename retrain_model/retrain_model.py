f2fd85ec4bb5e7868a80e087d8b7a87ac47b37e3
charan sai venkat narayana <94979200+lolugucharansai@users.noreply.github.com>
2024-04-21 07:57:02 +0000

    retrani.py

--- retrain_model/retrain_model.py
+++ retrain_model/retrain_model.py
@@ -13,11 +13,23 @@ args = parser.parse_args()
 data_path = args.diabetes_csv
 all_files = glob.glob(data_path + "/*.csv")
 data = pd.concat((pd.read_csv(f) for f in all_files), sort=False)
+X, y = data[
+        [
+            'Pregnancies',
+            'PlasmaGlucose',
+            'DiastolicBloodPressure',
+            'TricepsThickness',
+            'SerumInsulin',
+            'BMI',
+            'DiabetesPedigree',
+            'Age'
+        ]
+    ].values, data['Diabetic'].values
 
-X, y = data.drop('Outcome', axis=1), data['Outcome']
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # train/test split
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
 
 # Load the model from the .pkl file
 with open('./outputs/model.pkl', 'rb') as file:
