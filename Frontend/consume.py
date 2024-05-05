e91d593a633f0122dad59011bccd465b4dd38638
charan sai venkat narayana <94979200+lolugucharansai@users.noreply.github.com>
2024-05-05 17:12:00 +0530

    Update consume.py

--- Frontend/consume.py
+++ Frontend/consume.py
@@ -8,14 +8,10 @@ def allowSelfSignedHttps(allowed):
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+allowSelfSignedHttps(True)
 
 def predict_diabetes(pregnancies, plasma_glucose, diastolic_blood_pressure, triceps_thickness, serum_insulin, bmi, diabetes_pedigree, age):
-    # Request data goes here
-    # The example below assumes JSON formatting which may be updated
-    # depending on the format your endpoint expects.
-    # More information can be found here:
-    # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+
     data =  {
       "input_data": [
         [
@@ -35,19 +31,20 @@ def predict_diabetes(pregnancies, plasma_glucose, diastolic_blood_pressure, tric
     body = str.encode(json.dumps(data))
 
     url = 'https://diabetesend.eastus.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
+    # Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
     api_key = os.getenv('pass')
     if not api_key:
         raise Exception("A key should be provided to invoke the endpoint")
 
+    # The azureml-model-deployment header will force the request to go to a specific deployment.
+    # Remove this header to have the request observe the endpoint traffic rules
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'diabetesdeployment1' }
 
     req = urllib.request.Request(url, body, headers)
 
     try:
-       response = urllib.request.urlopen(req)
-
-       result = response.read()
-       
+        response = urllib.request.urlopen(req)
+        result = response.read()
     except urllib.error.HTTPError as error:
-      raise Exception("The request failed with status code: " + str(error.code))
+        raise Exception("The request failed with status code: " + str(error.code))
     return json.loads(result)[0]
